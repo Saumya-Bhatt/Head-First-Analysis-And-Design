506e803bd56dba95b587a2a98566280dc566f517
Saumya Bhatt <saumya.bhatt106@gmail.com>
2022-10-23 17:25:54 +0530

    Initial commit

--- DogDoor.kt
+++ DogDoor.kt
@@ -1,8 +1,22 @@
+import java.util.*
+import kotlin.concurrent.timerTask
+
 class DogDoor(
     private var open: Boolean = false
 ) {
   fun open() {
     this.open = true
+
+    /*
+    Schedule a task which will run after 500 milliseconds
+    The task will close the door and cancel the timer so that we don't go in a loop.
+    The door will now always close
+     */
+    val timer = Timer()
+    timer.schedule(timerTask {
+      close()  // even if we add new device, the door will always close
+      timer.cancel()
+    }, 5000)
   }
 
   fun close() {
